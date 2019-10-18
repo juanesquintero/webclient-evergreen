cd815e5d05db047e13f132018dc21a0809211839
juanesquintero <juaneschrome@gmail.com>
2019-10-18 17:41:31 -0500

    delete app.run()

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-# import requests
+import requests
 
 app = Flask(__name__, template_folder='templates')
 
 variables_list = ['PH','Temperatura','Humedad Tierra','Humedad Aire','Distancia','Luz Solar']
 
-sensores_list = []
 
 @app.route('/')
 def index():
-    return '<h1>TIPOS DE SENSORES</h1><p>Con estos enpoints puedes crear y ver los tipos de sensores</p><br>/CrearSensor  y  /listarSensores'
+    return '<h1>TIPOS DE SENSORES</h1><p>Con estos enpoints puedes crear y ver los tipos de sensores</p><br><p>/CrearSensor  y  /listarSensores</p>'
 
 @app.route('/crearSensor',methods=['GET'])
 def crearSensor():
@@ -17,13 +16,13 @@ def crearSensor():
 
 @app.route('/listarSensores',methods=['GET'])
 def listarSensores():
-    # sensores_list = requests.get('http://localhost:5000/tipoSensores').json()
+    sensores_list = requests.get('https://api-tiposensores.azurewebsites.net/tipoSensores').json()
     return render_template('listarSensores.html', sensores=sensores_list)
 
 @app.route('/guardarSensor',methods=['POST'])
 def guardarSensor():
     sensor = dict(request.values)
     sensor['precio'] = int(sensor['precio'])
-    # requests.post('http://localhost:5000/tipoSensores',json=sensor)
-    # sensores_list = requests.get('http://localhost:5000/tipoSensores').json()
-    return render_template('listarSensores.html', sensores=sensores_list)
+    requests.post('https://api-tiposensores.azurewebsites.net/tipoSensores',json=sensor)
+    
+    return listarSensores()
